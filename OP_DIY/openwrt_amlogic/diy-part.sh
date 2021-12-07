1fbe398cef7616fb36fdb232c78a76cb1f6318ac
281677160 <66651997+281677160@users.noreply.github.com>
2021-12-07 16:37:19 +0800

    Update diy-part.sh

--- OP_DIY/openwrt_amlogic/diy-part.sh
+++ OP_DIY/openwrt_amlogic/diy-part.sh
@@ -26,7 +26,7 @@ EOF
 
 sed -i 's/luci-theme-bootstrap/luci-theme-argon/g' feeds/luci/collections/luci/Makefile           # 选择argon为默认主题
 
-sed -i "s/OpenWrt /${Author} Compiled in $(TZ=UTC-8 date "+%Y.%m.%d") @ OpenWrt /g" $ZZZ          # 增加个性名字${Author}默认为你的github账号
+sed -i "s/OpenWrt /281677160 compiled in $(TZ=UTC-8 date "+%Y.%m.%d") @ OpenWrt /g" $ZZZ          # 增加个性名字(默认为281677160)
 
 sed -i '/CYXluq4wUazHjmCDBCqXF/d' $ZZZ                                                            # 设置密码为空
 
