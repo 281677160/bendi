f97a9dbbbf18d419bc29e7ce62f71354e9fdae40
281677160 <66651997+281677160@users.noreply.github.com>
2021-12-07 16:35:42 +0800

    Update diy-part.sh

--- OP_DIY/Lienol_source/diy-part.sh
+++ OP_DIY/Lienol_source/diy-part.sh
@@ -24,7 +24,7 @@ EOF
 
 sed -i 's/luci-theme-bootstrap/luci-theme-argon/g' feeds/luci/collections/luci/Makefile            # 选择argon为默认主题
 
-sed -i "s/OpenWrt /${Author} compiled in $(TZ=UTC-8 date "+%Y.%m.%d") @ OpenWrt /g" $ZZZ           # 增加个性名字 ${Author} 默认为你的github帐号
+sed -i "s/OpenWrt /281677160 compiled in $(TZ=UTC-8 date "+%Y.%m.%d") @ OpenWrt /g" $ZZZ           # 增加个性名字(默认为281677160)
 
 #sed -i 's/PATCHVER:=4.14/PATCHVER:=4.9/g' target/linux/x86/Makefile                               # x86机型,默认内核4.14，修改内核为4.9
 
