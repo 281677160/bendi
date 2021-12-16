40fb55ff9688ecaed22eed459114b51331eb8ea8
281677160 <66651997+281677160@users.noreply.github.com>
2021-12-16 23:10:59 +0800

    Update local.sh

--- local.sh
+++ local.sh
@@ -73,14 +73,15 @@ judge() {
     exit 1
   fi
 }
+
+if [[ "$USER" == "root" ]]; then
+  print_error "警告：请勿使用root用户编译，换一个普通用户吧~~"
+  exit 0
+fi
 export Ubname="$(. /etc/os-release && echo "$ID")"
 export xtbit=`getconf LONG_BIT`
 if [[ ! "${Ubname}" == "ubuntu" ]] && [[ ! "${xtbit}" == "64" ]]; then
   print_error "请使用Ubuntu 64位系统，推荐 Ubuntu 18 LTS 或 Ubuntu 20 LTS"
-  exit 0
-fi
-if [[ "$USER" == "root" ]]; then
-  print_error "警告：请勿使用root用户编译，换一个普通用户吧~~"
   exit 0
 fi
 
