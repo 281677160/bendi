d084ebea6fb000153bb7e98cd771bc7d5cd5f6ae
281677160 <66651997+281677160@users.noreply.github.com>
2021-12-16 15:53:03 +0800

    Update wsl.sh

--- wsl.sh
+++ wsl.sh
@@ -101,6 +101,8 @@ cd ${GITHUB_WORKSPACE}
   sudo apt-get update -y
   sudo apt-get install -y systemd build-essential asciidoc binutils bzip2 gawk gettext git libncurses5-dev libz-dev patch python3 python2.7 unzip zlib1g-dev lib32gcc1 libc6-dev-i386 lib32stdc++6 subversion flex uglifyjs git-core gcc-multilib p7zip p7zip-full msmtp libssl-dev texinfo libglib2.0-dev xmlto qemu-utils upx libelf-dev autoconf automake libtool autopoint device-tree-compiler g++-multilib antlr3 gperf wget curl rename libpcap0.8-dev swig rsync
   judge "部署编译环境"
+  sudo apt-get autoremove -y --purge
+  sudo apt-get clean -y
 }
 
 function op_kongjian() {
