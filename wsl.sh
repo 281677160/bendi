35a04891b29044df3106b0cd2a3b39d98332bba4
281677160 <66651997+281677160@users.noreply.github.com>
2022-03-19 23:58:45 +0800

    Update wsl.sh

--- wsl.sh
+++ wsl.sh
@@ -475,7 +475,7 @@ function op_make() {
   rm -rf build.log
   export START_TIME=`date +'%Y-%m-%d %H:%M:%S'`
   ECHOG "正在编译固件，请耐心等待..."
-  rm -fr ${COMFIRMWARE}/* > /dev/null 2>&1
+  [[ -d "${COMFIRMWARE}" ]] && rm -fr ${COMFIRMWARE}/*
   rm -rf ${Home}/{README,README.md,README_EN.md} > /dev/null 2>&1
   PATH=/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin make -j${npro} V=s 2>&1 |tee ${Home}/build.log
   if [[ "$(nproc)" -ge "16" ]];then
@@ -711,7 +711,7 @@ function openwrt_bgbg() {
       export START_TIME=`date +'%Y-%m-%d %H:%M:%S'`
       op_download
       ECHOG "编译固件"
-      rm -rf ${COMFIRMWARE}/*
+      [[ -d "${COMFIRMWARE}" ]] && rm -fr ${COMFIRMWARE}/*
       ./scripts/diffconfig.sh > ${GITHUB_WORKSPACE}/OP_DIY/${firmware}/${CONFIG_FILE}
       if [[ "$(nproc)" -ge "16" ]];then
         PATH=/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin make -j$(($(nproc) + 1)) V=s 2>&1 |tee ${Home}/build.log
