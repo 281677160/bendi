2e5f86d06eb93afc29f725f06d2c118cbbac651d
281677160 <66651997+281677160@users.noreply.github.com>
2022-03-19 14:46:27 +0800

    Update wsl.sh

--- wsl.sh
+++ wsl.sh
@@ -226,7 +226,7 @@ function feeds_clean() {
   git pull
   rm -rf ${Home}/package/{luci-app-passwall,luci-app-passwall2,luci-app-ssr-plus}
   ./scripts/feeds update -a > /dev/null 2>&1
-  cp -rf ${Home}/zdefault-settings ${ZZZ}
+  cp -Rf ${Home}/zdefault-settings ${ZZZ}
   rm -rf ${Builb}/common && git clone https://github.com/281677160/common ${Builb}/common
   judge "额外扩展脚本下载"
   chmod -R +x ${Builb}/common
