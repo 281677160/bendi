a09547110c1562e6dbd1fea928ce559ffef9dd97
281677160 <66651997+281677160@users.noreply.github.com>
2022-03-19 17:29:08 +0800

    Update diy-part.sh

--- OP_DIY/Lienol_source/diy-part.sh
+++ OP_DIY/Lienol_source/diy-part.sh
@@ -34,7 +34,7 @@ sed -i "s/OpenWrt /281677160 compiled in $(TZ=UTC-8 date "+%Y.%m.%d") @ OpenWrt 
 cat >$DELETE <<-EOF
 EOF
 
-# 修改插件名字
+# 修改插件名字(本地编译保存缓存二次编译不使用)
 sed -i 's/"aMule设置"/"电驴下载"/g' `grep "aMule设置" -rl ./`
 sed -i 's/"网络存储"/"NAS"/g' `grep "网络存储" -rl ./`
 sed -i 's/"Turbo ACC 网络加速"/"网络加速"/g' `grep "Turbo ACC 网络加速" -rl ./`
