175f2ee767bedc63785e6d23759da7334be46264
281677160 <66651997+281677160@users.noreply.github.com>
2022-03-19 23:38:01 +0800

    Update wsl.sh

--- wsl.sh
+++ wsl.sh
@@ -620,7 +620,7 @@ function op_firmware() {
     export ZZZ="${Home}/package/lean/default-settings/files/zzz-default-settings"
     export Diy_zdy="Diy_lede"
     export OpenWrt_name="18.06"
-    echo "jilu" > "${Home}/.Lede_core"
+    echo "jilu" > "${Home}/.Lede_core" > /dev/null 2>&1
   fi
   if [[ "${firmware}" == "Lienol_source" ]] || [[ -n "$(ls -A "${Home}/.Lienol_core" 2>/dev/null)" ]]; then
     export firmware="Lienol_source"
@@ -631,7 +631,7 @@ function op_firmware() {
     export ZZZ="${Home}/package/default-settings/files/zzz-default-settings"
     export Diy_zdy="Diy_lienol"
     export OpenWrt_name="20.06"
-    echo "jilu" > "${Home}/.Lienol_core"
+    echo "jilu" > "${Home}/.Lienol_core" > /dev/null 2>&1
   fi
   if [[ "${firmware}" == "Mortal_source" ]] || [[ -n "$(ls -A "${Home}/.Mortal_core" 2>/dev/null)" ]]; then
     export firmware="Mortal_source"
@@ -642,7 +642,7 @@ function op_firmware() {
     export ZZZ="${Home}/package/emortal/default-settings/files/99-default-settings"
     export Diy_zdy="Diy_mortal"
     export OpenWrt_name="21.02"
-    echo "jilu" > "${Home}/.Mortal_core"
+    echo "jilu" > "${Home}/.Mortal_core" > /dev/null 2>&1
   fi
   if [[ "${firmware}" == "Tianling_source" ]] || [[ -n "$(ls -A "${Home}/.Tianling_core" 2>/dev/null)" ]]; then
     export firmware="Tianling_source"
@@ -653,7 +653,7 @@ function op_firmware() {
     export ZZZ="${Home}/package/emortal/default-settings/files/99-default-settings"
     export Diy_zdy="Diy_Tianling"
     export OpenWrt_name="18.06_tl"
-    echo "jilu" > "${Home}/.Tianling_core"
+    echo "jilu" > "${Home}/.Tianling_core" > /dev/null 2>&1
   fi
   if [[ "${firmware}" == "openwrt_amlogic" ]] || [[ -n "$(ls -A "${Home}/.amlogic_core" 2>/dev/null)" ]]; then
     export firmware="openwrt_amlogic"
@@ -664,7 +664,7 @@ function op_firmware() {
     export ZZZ="${Home}/package/lean/default-settings/files/zzz-default-settings"
     export Diy_zdy="Diy_lede"
     export OpenWrt_name="18.06"
-    echo "jilu" > "${Home}/.amlogic_core"
+    echo "jilu" > "${Home}/.amlogic_core" > /dev/null 2>&1
   fi
 }
 
