c1cd6284ce23b23ba9ebaf04d8a787dd230a7f02
281677160 <66651997+281677160@users.noreply.github.com>
2022-03-26 02:51:32 +0800

    Update diy-part.sh

--- OP_DIY/Tianling_source/diy-part.sh
+++ OP_DIY/Tianling_source/diy-part.sh
@@ -10,9 +10,9 @@ sed -i 's/luci-theme-bootstrap/luci-theme-argon/g' feeds/luci/collections/luci/M
 
 sed -i "s/OpenWrt /281677160 compiled in $(TZ=UTC-8 date "+%Y.%m.%d") @ OpenWrt /g" $ZZZ_PATH       # 增加个性名字(默认为281677160)
 
-sed -i '/CYXluq4wUazHjmCDBCqXF/d' $ZZZ                                                              # 设置密码为空
+sed -i '/CYXluq4wUazHjmCDBCqXF/d' $ZZZ_PATH                                                         # 设置密码为空
 
-#sed -i 's/PATCHVER:=4.19/PATCHVER:=4.14/g' target/linux/x86/Makefile                                # x86机型,默认内核4.19，修改内核为4.14或者4.9
+#sed -i 's/PATCHVER:=4.19/PATCHVER:=4.14/g' target/linux/x86/Makefile                               # x86机型,默认内核4.19，修改内核为4.14或者4.9
 
 
 # K3专用，编译K3的时候只会出K3固件
