05077e695ff65395aa9bef5efae1c02b825dd4f1
281677160 <66651997+281677160@users.noreply.github.com>
2022-03-27 11:51:02 +0800

    Update wsl.sh

--- wsl.sh
+++ wsl.sh
@@ -446,6 +446,11 @@ function op_amlogic() {
        git clone --depth 1 https://github.com/ophub/amlogic-s9xxx-openwrt.git ${GITHUB_WORKSPACE}/amlogic
        judge "内核运行文件下载"
        rm -rf ${GITHUB_WORKSPACE}/amlogic/{router-config,LICENSE,README.cn.md,README.md,.github,.git}
+  else
+       ECHOY "正在下载打包所需的程序,请耐心等候~~~"
+       git clone --depth 1 https://github.com/ophub/amlogic-s9xxx-openwrt.git ${GITHUB_WORKSPACE}/amlogic
+       judge "内核运行文件下载"
+       rm -rf ${GITHUB_WORKSPACE}/amlogic/{router-config,LICENSE,README.cn.md,README.md,.github,.git}
   fi
   [ -d amlogic/openwrt-armvirt ] || mkdir -p amlogic/openwrt-armvirt
   ECHOY "全部可打包机型：s905x3_s905x2_s905x_s905w_s905d_s922x_s912"
