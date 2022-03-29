ef7894138de6d77f0b72fd77d916c45666d1f8be
281677160 <66651997+281677160@users.noreply.github.com>
2022-03-29 15:36:26 +0800

    Update wsl.sh

--- wsl.sh
+++ wsl.sh
@@ -545,47 +545,33 @@ function op_end() {
 function op_firmware() {
   if [[ "${matrixtarget}" == "Lede_source" ]] || [[ -n "$(ls -A "${HOME_PATH}/.Lede_core" 2>/dev/null)" ]]; then
     export matrixtarget="Lede_source"
-    export SOURCE="Lede"
+    export BUILD_PATH="${GITHUB_WORKSPACE}/openwrt/build/${matrixtarget}"
+    source "${BUILD_PATH}/common.sh" && Bendi_variable
     export Mark_Core=".Lede_core"
-    export BUILD_PATH="${GITHUB_WORKSPACE}/openwrt/build/${matrixtarget}"
-    export ZZZ_PATH="${HOME_PATH}/package/lean/default-settings/files/zzz-default-settings"
-    export LUCI_EDITION="18.06"
     [[ -d "${HOME_PATH}" ]] && echo "${Mark_Core}" > "${HOME_PATH}/${Mark_Core}"
-  fi
-  if [[ "${matrixtarget}" == "Lienol_source" ]] || [[ -n "$(ls -A "${HOME_PATH}/.Lienol_core" 2>/dev/null)" ]]; then
+  elif [[ "${matrixtarget}" == "Lienol_source" ]] || [[ -n "$(ls -A "${HOME_PATH}/.Lienol_core" 2>/dev/null)" ]]; then
     export matrixtarget="Lienol_source"
-    export SOURCE="Lienol"
+    export BUILD_PATH="${GITHUB_WORKSPACE}/openwrt/build/${matrixtarget}"
+    source "${BUILD_PATH}/common.sh" && Bendi_variable
     export Mark_Core=".Lienol_core"
-    export BUILD_PATH="${GITHUB_WORKSPACE}/openwrt/build/${matrixtarget}"
-    export ZZZ_PATH="${HOME_PATH}/package/default-settings/files/zzz-default-settings"
-    export LUCI_EDITION="20.07"
     [[ -d "${HOME_PATH}" ]] && echo "${Mark_Core}" > "${HOME_PATH}/${Mark_Core}"
-  fi
-  if [[ "${matrixtarget}" == "Tianling_source" ]] || [[ -n "$(ls -A "${HOME_PATH}/.Tianling_core" 2>/dev/null)" ]]; then
+  elif [[ "${matrixtarget}" == "Tianling_source" ]] || [[ -n "$(ls -A "${HOME_PATH}/.Tianling_core" 2>/dev/null)" ]]; then
     export matrixtarget="Tianling_source"
-    export SOURCE="Tianling"
+    export BUILD_PATH="${GITHUB_WORKSPACE}/openwrt/build/${matrixtarget}"
+    source "${BUILD_PATH}/common.sh" && Bendi_variable
     export Mark_Core=".Tianling_core"
-    export BUILD_PATH="${GITHUB_WORKSPACE}/openwrt/build/${matrixtarget}"
-    export ZZZ_PATH="${HOME_PATH}/package/emortal/default-settings/files/99-default-settings"
-    export LUCI_EDITION="18.06"
     [[ -d "${HOME_PATH}" ]] && echo "${Mark_Core}" > "${HOME_PATH}/${Mark_Core}"
-  fi
-  if [[ "${matrixtarget}" == "Mortal_source" ]] || [[ -n "$(ls -A "${HOME_PATH}/.Mortal_core" 2>/dev/null)" ]]; then
+  elif [[ "${matrixtarget}" == "Mortal_source" ]] || [[ -n "$(ls -A "${HOME_PATH}/.Mortal_core" 2>/dev/null)" ]]; then
     export matrixtarget="Mortal_source"
-    export SOURCE="Mortal"
+    export BUILD_PATH="${GITHUB_WORKSPACE}/openwrt/build/${matrixtarget}"
+    source "${BUILD_PATH}/common.sh" && Bendi_variable
     export Mark_Core=".Mortal_core"
-    export BUILD_PATH="${GITHUB_WORKSPACE}/openwrt/build/${matrixtarget}"
-    export ZZZ_PATH="${HOME_PATH}/package/emortal/default-settings/files/99-default-settings"
-    export LUCI_EDITION="21.02"
     [[ -d "${HOME_PATH}" ]] && echo "${Mark_Core}" > "${HOME_PATH}/${Mark_Core}"
-  fi
-  if [[ "${matrixtarget}" == "openwrt_amlogic" ]] || [[ -n "$(ls -A "${HOME_PATH}/.amlogic_core" 2>/dev/null)" ]]; then
+  elif [[ "${matrixtarget}" == "openwrt_amlogic" ]] || [[ -n "$(ls -A "${HOME_PATH}/.amlogic_core" 2>/dev/null)" ]]; then
     export matrixtarget="openwrt_amlogic"
-    export SOURCE="Lede"
-    export Mark_Core=".amlogic_core"
     export BUILD_PATH="${GITHUB_WORKSPACE}/openwrt/build/${matrixtarget}"
-    export ZZZ_PATH="${HOME_PATH}/package/lean/default-settings/files/zzz-default-settings"
-    export LUCI_EDITION="18.06"
+    source "${BUILD_PATH}/common.sh" && Bendi_variable
+    export Mark_Core=".amlogic_core"
     [[ -d "${HOME_PATH}" ]] && echo "${Mark_Core}" > "${HOME_PATH}/${Mark_Core}"
   fi
 }
