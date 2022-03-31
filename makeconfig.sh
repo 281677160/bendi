ae530cbaa880dd20c963e89314d7a4298360ed42
281677160 <66651997+281677160@users.noreply.github.com>
2022-03-31 18:40:25 +0800

    Update makeconfig.sh

--- makeconfig.sh
+++ makeconfig.sh
@@ -121,7 +121,7 @@ cd ${GITHUB_WORKSPACE}
   echo
   sudo apt-get update -y
   sudo apt-get full-upgrade -y
-  sudo apt-get install -y systemd build-essential asciidoc binutils bzip2 gawk gettext git libncurses5-dev libz-dev patch python3 python2.7 unzip zlib1g-dev lib32gcc1 libc6-dev-i386 lib32stdc++6 subversion flex uglifyjs git-core gcc-multilib p7zip p7zip-full msmtp libssl-dev texinfo libglib2.0-dev xmlto qemu-utils upx libelf-dev autoconf automake libtool autopoint device-tree-compiler g++-multilib antlr3 gperf wget curl rename libpcap0.8-dev swig rsync
+  sudo -E apt-get -qq install -y git subversion git-core wget curl grep
   judge "部署编译环境"
   sudo apt-get autoremove -y --purge > /dev/null 2>&1
   sudo apt-get clean -y > /dev/null 2>&1
@@ -163,7 +163,7 @@ function op_diywenjian() {
     rm -rf ${GITHUB_WORKSPACE}/CONFIG_DIY/*/.config
     if [[ -d ${GITHUB_WORKSPACE}/CONFIG_DIY ]]; then
       rm -rf bendi && git clone https://github.com/281677160/common bendi
-      judge  "OP_DIY文件下载"
+      judge  "CONFIG_DIY文件下载"
       cp -Rf ${GITHUB_WORKSPACE}/bendi/CONFIG_DIY/* ${GITHUB_WORKSPACE}/CONFIG_DIY/
     else
       print_error "CONFIG_DIY文件下载失败"
@@ -227,14 +227,14 @@ function op_repo_branch() {
 
 function op_jiaoben() {
   if [[ ! -d ${HOME_PATH}/build ]]; then
-    cp -Rf ${GITHUB_WORKSPACE}/OP_DIY ${HOME_PATH}/build
+    cp -Rf ${GITHUB_WORKSPACE}/CONFIG_DIY ${HOME_PATH}/build
   else
-    cp -Rf ${GITHUB_WORKSPACE}/OP_DIY/* ${HOME_PATH}/build/
+    cp -Rf ${GITHUB_WORKSPACE}/CONFIG_DIY/* ${HOME_PATH}/build/
   fi
   [[ "${Tishi}" == "1" ]] && sed -i '/-rl/d' "${BUILD_PATH}/${DIY_PART_SH}"
   rm -rf ${HOME_PATH}/build/common && git clone https://github.com/281677160/common ${HOME_PATH}/build/common
   judge "额外扩展文件下载"
-  rm -rf ${HOME_PATH}/build/common/OP_DIY
+  rm -rf ${HOME_PATH}/build/common/CONFIG_DIY
   mv -f ${LOCAL_Build}/common/*.sh ${BUILD_PATH}
   chmod -R +x ${BUILD_PATH}
   source "${BUILD_PATH}/common.sh" && Bendi_variable
@@ -286,290 +286,34 @@ function make_defconfig() {
     ;;
     *)
       rm -rf ${HOME_PATH}/{CHONGTU,Chajianlibiao,EXT4}
-      ECHOG "继续编译中...！"
+      ECHOG "正在制作配置文件...！"
     ;;
     esac
   fi
   source "${BUILD_PATH}/common.sh" && Diy_menu2
-}
-
-function tixing_op_config() {
-  export TARGET_BOARD="$(awk -F '[="]+' '/TARGET_BOARD/{print $2}' "${GITHUB_WORKSPACE}/OP_DIY/${matrixtarget}/${CONFIG_FILE}")"
-  export TARGET_SUBTARGET="$(awk -F '[="]+' '/TARGET_SUBTARGET/{print $2}' "${GITHUB_WORKSPACE}/OP_DIY/${matrixtarget}/${CONFIG_FILE}")"
-  if [[ `grep -c "CONFIG_TARGET_x86_64=y" "${GITHUB_WORKSPACE}/OP_DIY/${matrixtarget}/${CONFIG_FILE}"` -eq '1' ]]; then
-    export TARGET_PROFILE="x86-64"
-  elif [[ `grep -c "CONFIG_TARGET_x86=y" ${GITHUB_WORKSPACE}/OP_DIY/${matrixtarget}/${CONFIG_FILE}` == '1' ]] && [[ `grep -c "CONFIG_TARGET_x86_64=y" "${GITHUB_WORKSPACE}/OP_DIY/${matrixtarget}/${CONFIG_FILE}"` == '0' ]]; then
-    export TARGET_PROFILE="x86_32"
-  elif [[ `grep -c "CONFIG_TARGET.*DEVICE.*=y" "${GITHUB_WORKSPACE}/OP_DIY/${matrixtarget}/${CONFIG_FILE}"` -eq '1' ]]; then
-    export TARGET_PROFILE="$(egrep -o "CONFIG_TARGET.*DEVICE.*=y" "${GITHUB_WORKSPACE}/OP_DIY/${matrixtarget}/${CONFIG_FILE}" | sed -r 's/.*DEVICE_(.*)=y/\1/')"
-  else
-    export TARGET_PROFILE="$(awk -F '[="]+' '/TARGET_BOARD/{print $2}' ${GITHUB_WORKSPACE}/OP_DIY/${matrixtarget}/${CONFIG_FILE})"
-  fi
-  export TARGET_BSGET="$HOME_PATH/bin/targets/$TARGET_BOARD/$TARGET_SUBTARGET"
-  [[ -z "${TARGET_PROFILE}" ]] && TARGET_PROFILE="OP_DIY/${matrixtarget}没有${CONFIG_FILE}文件,或者${CONFIG_FILE}文件内容为空"
-}
-
-function chenggong_op_config() {
-  if [[ `grep -c "CONFIG_TARGET_x86_64=y" "${BUILD_PATH}/.config"` -eq '1' ]]; then
-    export CG_PROFILE="x86-64"
-  elif [[ `grep -c "CONFIG_TARGET_x86=y" ${BUILD_PATH}/.config` == '1' ]] && [[ `grep -c "CONFIG_TARGET_x86_64=y" "${BUILD_PATH}/.config"` == '0' ]]; then
-    export CG_PROFILE="x86_32"
-  elif [[ `grep -c "CONFIG_TARGET.*DEVICE.*=y" "${BUILD_PATH}/.config"` -eq '1' ]]; then
-    export CG_PROFILE="$(egrep -o "CONFIG_TARGET.*DEVICE.*=y" "${BUILD_PATH}/.config" | sed -r 's/.*DEVICE_(.*)=y/\1/')"
-  else
-    export CG_PROFILE="$(awk -F '[="]+' '/TARGET_BOARD/{print $2}' ${BUILD_PATH}/.config)"
-  fi
-  [[ -z "${CG_PROFILE}" ]] && CG_PROFILE="未知"
-}
-
-function op_upgrade2() {
-  cd ${HOME_PATH}
-  export Upgrade_Date="$(date +%Y%m%d%H%M)"
-  if [ "${REGULAR_UPDATE}" == "true" ]; then
-    source ${BUILD_PATH}/upgrade.sh && Diy_Part2
-  fi
-}
-
-function op_download() {
-  cd ${HOME_PATH}
-  ECHOG "下载DL文件，请耐心等候..."
-  rm -rf ${HOME_PATH}/build.log
-  if [[ ${Tishi} == "1" ]]; then
-    make -j2 download 2>&1 |tee ${HOME_PATH}/build.log
-  else
-    make -j8 download 2>&1 |tee ${HOME_PATH}/build.log
-  fi
-  find dl -size -1024c -exec ls -l {} \;
-  find dl -size -1024c -exec rm -f {} \;
-  if [[ `grep -c "make with -j1 V=s or V=sc" ${HOME_PATH}/build.log` == '0' ]] || [[ `grep -c "ERROR" ${HOME_PATH}/build.log` == '0' ]]; then
-    print_ok "DL文件下载成功"
-  else
-    clear
-    echo
-    print_error "下载DL失败，更换节点后再尝试下载？"
-    QLMEUN="请更换节点后按[Y/y]回车继续尝试下载DL，或输入[N/n]回车,退出编译"
-    while :; do
-      read -p " [${QLMEUN}]： " XZDLE
-      case $XZDLE in
-      [Yy])
-        op_download
-      break
-      ;;
-      [Nn])
-        ECHOR "退出编译程序!"
-        sleep 2
-        exit 1
-       break
-       ;;
-       *)
-         QLMEUN="请更换节点后按[Y/y]回车继续尝试下载DL，或现在输入[N/n]回车,退出编译"
-       ;;
-       esac
-    done
-  fi
-}
-
-function op_cpuxinghao() {
-  cd ${HOME_PATH}
-  rm -rf build.log
-  Model_Name="$(cat /proc/cpuinfo |grep 'model name' |awk 'END {print}' |cut -f2 -d: |sed 's/^[ ]*//g')"
-  Cpu_Cores="$(cat /proc/cpuinfo | grep 'cpu cores' |awk 'END {print}' | cut -f2 -d: | sed 's/^[ ]*//g')"
-  clear
-  ECHOG "您的CPU型号为[ ${Model_Name} ]"
-  ECHOG "在Ubuntu使用核心数为[ ${Cpu_Cores} ],线程数为[ $(nproc) ]"
-  if [[ "$(nproc)" == "1" ]]; then
-    ECHOY "正在使用[$(nproc)线程]编译固件,预计要[3.5]小时左右,请耐心等待..."
-  elif [[ "$(nproc)" =~ (2|3) ]]; then
-    ECHOY "正在使用[$(nproc)线程]编译固件,预计要[3]小时左右,请耐心等待..."
-  elif [[ "$(nproc)" =~ (4|5) ]]; then
-    ECHOY "正在使用[$(nproc)线程]编译固件,预计要[2.5]小时左右,请耐心等待..."
-  elif [[ "$(nproc)" =~ (6|7) ]]; then
-    ECHOY "正在使用[$(nproc)线程]编译固件,预计要[2]小时左右,请耐心等待..."
-  elif [[ "$(nproc)" =~ (8|9) ]]; then
-    ECHOY "正在使用[$(nproc)线程]编译固件,预计要[1.5]小时左右,请耐心等待..."
-  else
-    ECHOY "正在使用[$(nproc)线程]编译固件,预计要[1]小时左右,请耐心等待..."
-    ECHOG "若您的CPU线程数超过16线程的话，强制使用16线程编译"
-  fi
-  sleep 3
-}
-
-function op_make() {
-  cd ${HOME_PATH}
-  rm -rf build.log
-  export START_TIME=`date +'%Y-%m-%d %H:%M:%S'`
-  ECHOG "正在编译固件，请耐心等待..."
-  [[ -d "${TARGET_BSGET}" ]] && rm -fr ${TARGET_BSGET}/*
-  rm -rf ${HOME_PATH}/{README,README.md,README_EN.md} > /dev/null 2>&1
-  ./scripts/diffconfig.sh > ${GITHUB_WORKSPACE}/OP_DIY/${matrixtarget}/${CONFIG_FILE}
-  if [[ "${WSL_ubuntu}" == "YES" ]]; then
-    if [[ "$(nproc)" -ge "16" ]];then
-      PATH=/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin make -j$(($(nproc) + 1)) V=s 2>&1 |tee ${HOME_PATH}/build.log
-    else
-      PATH=/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin make -j16 V=s 2>&1 |tee ${HOME_PATH}/build.log
-    fi
-  else
-    if [[ "$(nproc)" -ge "16" ]];then
-      make -j$(($(nproc) + 1)) V=s 2>&1 |tee ${HOME_PATH}/build.log
-    else
-      make -j16 V=s 2>&1 |tee ${HOME_PATH}/build.log
-    fi
-  fi
-  if [[ `ls -a ${TARGET_BSGET} | grep -c "${TARGET_BOARD}"` == '0' ]]; then
-    rm -rf ${LOCAL_Build}/chenggong > /dev/null 2>&1
-    echo "shibai" >${LOCAL_Build}/shibai
-    print_error "编译失败~~!"
-    print_error "请用工具把openwrt文件夹里面的[build.log]日志文件拖至电脑，然后查找失败原因"
-    sleep 1
-    exit 1
-  else
-    rm -rf ${LOCAL_Build}/shibai > /dev/null 2>&1
-    echo "chenggong" >${LOCAL_Build}/chenggong
-    rm -rf ${HOME_PATH}/build.log
-    ./scripts/diffconfig.sh > ${BUILD_PATH}/.config
-  fi
+  ./scripts/diffconfig.sh > "${GITHUB_WORKSPACE}/CONFIG_DIY/${matrixtarget}/${CONFIG_FILE}"
 }
 
 function op_upgrade3() {
   cd ${HOME_PATH}
-  if [[ "${REGULAR_UPDATE}" == "true" ]]; then
-    [[ -d "${HOME_PATH}/bin/Firmware" ]] && rm -fr ${HOME_PATH}/bin/Firmware/*
-    [[ -d "${HOME_PATH}/upgrade" ]] && rm -rf ${HOME_PATH}/upgrade
-    cp -Rf ${TARGET_BSGET} ${HOME_PATH}/upgrade
-    source ${BUILD_PATH}/upgrade.sh && Diy_Part3
-  fi
-  if [[ `ls -a ${HOME_PATH}/bin/Firmware | grep -c "${Upgrade_Date}"` -ge '1' ]]; then
-    print_ok "加入‘定时升级插件的固件’操作完成"
-    export dsgx="加入‘定时升级插件的固件’已经放入[bin/Firmware]文件夹中"
-  else
-    export dsgx="加入‘定时升级固件插件’的固件失败，您的机型或者不支持定时更新!"
-  fi
-  cd ${TARGET_BSGET}
-  rename -v "s/^immortalwrt/openwrt/" * > /dev/null 2>&1
-  if [[ -f ${GITHUB_WORKSPACE}/Clear ]]; then
-    cp -Rf ${GITHUB_WORKSPACE}/Clear ${TARGET_BSGET}/Clear.sh
-    chmod +x Clear.sh && source Clear.sh
-    rm -rf Clear.sh
-  fi
-  rename -v "s/^openwrt/${SOURCE}/" * > /dev/null 2>&1
-  cd ${HOME_PATH}
+  print_ok "配置文件制作完成，已经覆盖进[CONFIG_DIY/${matrixtarget}/${CONFIG_FILE}]文件中"
+  exit 0
 }
 
-function op_amlogic() {
-  cd ${GITHUB_WORKSPACE}
-  if [[ `ls -1 "${HOME_PATH}/bin/targets/armvirt/64" | grep -c "tar.gz"` == '0' ]]; then
-    mkdir -p "${HOME_PATH}/bin/targets/armvirt/64"
-    clear
-    echo
-    echo
-    echo
-    ECHOR "没发现您的 openwrt/bin/targets/armvirt/64 文件夹里存在.tar.gz固件，已为你创建了文件夹"
-    ECHORR "请先将\"openwrt-armvirt-64-default-rootfs.tar.gz\"固件"
-    ECHOR "存入 openwrt/bin/targets/armvirt/64 文件夹里面，再使用命令进行打包"
-    echo
-    [[ "${WSL_ubuntu}" == "YES" ]] && explorer.exe .
-    exit 1
-  fi
-  if [[ ! -d "/home/dan/amlogic" ]]; then
-    ECHOY "正在下载打包所需的程序,请耐心等候~~~"
-    git clone --depth 1 https://github.com/ophub/amlogic-s9xxx-openwrt.git ${GITHUB_WORKSPACE}/amlogic
-    judge "内核运行文件下载"
-    rm -rf ${GITHUB_WORKSPACE}/amlogic/{router-config,LICENSE,README.cn.md,README.md,.github,.git}
-  elif [[ -d "${GITHUB_WORKSPACE}/amlogic" ]] && [[  ! -f "${GITHUB_WORKSPACE}/amlogic/make" ]]; then
-    ECHOGG "发现已存在的打包程序缺少文件，请输入ubuntu密码删除打包程序重新下载"
-    sudo rm -rf "${GITHUB_WORKSPACE}/amlogic"
-    ECHOY "正在下载打包所需的程序,请耐心等候~~~"
-    git clone --depth 1 https://github.com/ophub/amlogic-s9xxx-openwrt.git ${GITHUB_WORKSPACE}/amlogic
-    judge "内核运行文件下载"
-    rm -rf ${GITHUB_WORKSPACE}/amlogic/{router-config,LICENSE,README.cn.md,README.md,.github,.git}
-  elif [[ -d "${GITHUB_WORKSPACE}/amlogic" ]] && [[  ! -d "${GITHUB_WORKSPACE}/amlogic/amlogic-s9xxx" ]]; then
-    ECHOGG "发现已存在的打包程序缺少文件，请输入ubuntu密码删除打包程序重新下载"
-    sudo rm -rf "${GITHUB_WORKSPACE}/amlogic"
-    ECHOY "正在下载打包所需的程序,请耐心等候~~~"
-    git clone --depth 1 https://github.com/ophub/amlogic-s9xxx-openwrt.git ${GITHUB_WORKSPACE}/amlogic
-    judge "内核运行文件下载"
-    rm -rf ${GITHUB_WORKSPACE}/amlogic/{router-config,LICENSE,README.cn.md,README.md,.github,.git}
-  fi
-  [ -d amlogic/openwrt-armvirt ] || mkdir -p amlogic/openwrt-armvirt
-  ECHOY "全部可打包机型：s905x3_s905x2_s905x_s905w_s905d_s922x_s912"
-  ECHOGG "设置要打包固件的机型[ 直接回车则默认全部机型 ]"
-  export root_size="$(egrep -o ROOT_MB=\"[0-9]+\" "$GITHUB_WORKSPACE/amlogic/make" |cut -d "=" -f2 |sed 's/\"//g' )"
-  read -p " 请输入您要设置的机型：" amlogic_model
-  export amlogic_model=${amlogic_model:-"s905x3_s905x2_s905x_s905w_s905d_s922x_s912"}
-  ECHOYY "您设置的机型为：${amlogic_model}"
-  echo
-  ECHOGG "设置打包的内核版本[直接回车则默认自动检测最新内核]"
-  read -p " 请输入您要设置的内核：" amlogic_kernel
-  export amlogic_kernel=${amlogic_kernel:-"5.10.100_5.4.180 -a true"}
-  if [[ "${amlogic_kernel}" == "5.10.100_5.4.180 -a true" ]]; then
-    ECHOYY "您设置的内核版本为：自动检测最新版内核打包"
+function tixing_op_config() {
+  export TARGET_BOARD="$(awk -F '[="]+' '/TARGET_BOARD/{print $2}' "${GITHUB_WORKSPACE}/CONFIG_DIY/${matrixtarget}/${CONFIG_FILE}")"
+  export TARGET_SUBTARGET="$(awk -F '[="]+' '/TARGET_SUBTARGET/{print $2}' "${GITHUB_WORKSPACE}/CONFIG_DIY/${matrixtarget}/${CONFIG_FILE}")"
+  if [[ `grep -c "CONFIG_TARGET_x86_64=y" "${GITHUB_WORKSPACE}/CONFIG_DIY/${matrixtarget}/${CONFIG_FILE}"` -eq '1' ]]; then
+    export TARGET_PROFILE="x86-64"
+  elif [[ `grep -c "CONFIG_TARGET_x86=y" ${GITHUB_WORKSPACE}/CONFIG_DIY/${matrixtarget}/${CONFIG_FILE}` == '1' ]] && [[ `grep -c "CONFIG_TARGET_x86_64=y" "${GITHUB_WORKSPACE}/CONFIG_DIY/${matrixtarget}/${CONFIG_FILE}"` == '0' ]]; then
+    export TARGET_PROFILE="x86_32"
+  elif [[ `grep -c "CONFIG_TARGET.*DEVICE.*=y" "${GITHUB_WORKSPACE}/CONFIG_DIY/${matrixtarget}/${CONFIG_FILE}"` -eq '1' ]]; then
+    export TARGET_PROFILE="$(egrep -o "CONFIG_TARGET.*DEVICE.*=y" "${GITHUB_WORKSPACE}/CONFIG_DIY/${matrixtarget}/${CONFIG_FILE}" | sed -r 's/.*DEVICE_(.*)=y/\1/')"
   else
-    ECHOYY "您设置的内核版本为：${amlogic_kernel}"
+    export TARGET_PROFILE="$(awk -F '[="]+' '/TARGET_BOARD/{print $2}' ${GITHUB_WORKSPACE}/CONFIG_DIY/${matrixtarget}/${CONFIG_FILE})"
   fi
-  echo
-  ECHOGG "设置ROOTFS分区大小[ 直接回车则默认：${root_size} ]"
-  read -p " 请输入ROOTFS分区大小：" rootfs_size
-  export rootfs_size=${rootfs_size:-"${root_size}"}
-  ECHOYY "您设置的ROOTFS分区大小为：${rootfs_size}"
-  export make_size="$(egrep -o ROOT_MB=\"[0-9]+\" "$GITHUB_WORKSPACE/amlogic/make")"
-  export zhiding_size="ROOT_MB=\"${rootfs_size}\""
-  sed -i "s?${make_size}?${zhiding_size}?g" "$GITHUB_WORKSPACE/amlogic/make"
-  echo
-  ECHOGG "请输入ubuntu密码进行固件打包程序"
-  sudo rm -rf ${GITHUB_WORKSPACE}/amlogic/out/*
-  sudo rm -rf ${GITHUB_WORKSPACE}/amlogic/openwrt-armvirt/*
-  sudo rm -rf ${GITHUB_WORKSPACE}/amlogic/amlogic-s9xxx/amlogic-kernel/*
-  cp -Rf ${HOME_PATH}/bin/targets/armvirt/64/*.tar.gz ${GITHUB_WORKSPACE}/amlogic/openwrt-armvirt/openwrt-armvirt-64-default-rootfs.tar.gz && sync
-  if [[ `ls -a amlogic/openwrt-armvirt | grep -c "openwrt-armvirt-64-default-rootfs.tar.gz"` == '0' ]]; then
-    print_error "amlogic/openwrt-armvirt文件夹没发现openwrt-armvirt-64-default-rootfs.tar.gz固件存在"
-    print_error "请检查${HOME_PATH}/bin/targets/armvirt/64文件夹内有没有openwrt-armvirt-64-default-rootfs.tar.gz固件存在"
-    exit 1
-  fi
-  cd amlogic
-  sudo chmod +x make
-  sudo ./make -d -b ${amlogic_model} -k ${amlogic_kernel}
-  if [[ `ls -a ${GITHUB_WORKSPACE}/amlogic/out | grep -c "openwrt"` -ge '1' ]]; then
-    print_ok "打包完成，固件存放在[amlogic/out]文件夹"
-    [[ "${WSL_ubuntu}" == "YES" ]] && explorer.exe .
-  else
-    print_error "打包失败，请再次尝试!"
-  fi
-  [[ -d ${HOME_PATH} ]] && cd ${HOME_PATH}
-}
-
-function op_end() {
-  clear
-  echo
-  echo
-  cd ${HOME_PATH}
-  if [[ ${matrixtarget} == "openwrt_amlogic" ]]; then
-    print_ok "使用[ ${matrixtarget} ]文件夹，编译[ N1和晶晨系列盒子专用固件 ]顺利编译完成~~~"
-  else
-    print_ok "使用[ ${matrixtarget} ]文件夹，编译[ ${TARGET_PROFILE} ]顺利编译完成~~~"
-  fi
-  ECHOY "后台地址: ${IP}"
-  ECHOY "用户名: root"
-  ECHOY "固件已经存入${TARGET_OPENWRT}文件夹中"
-  [[ "${REGULAR_UPDATE}" == "true" ]] && ECHOY "${dsgx}"
-  if [[ "${matrixtarget}" == "openwrt_amlogic" ]]; then
-    ECHOR "提示：再次输入编译命令可选择二次编译或者打包N1和晶晨系列盒子专用固件"
-  else
-    ECHOR "提示：再次输入编译命令可进行二次编译"
-  fi
-  [[ "${WSL_ubuntu}" == "YES" ]] && explorer.exe .
-  ECHOY "编译日期：$(date +'%Y年%m月%d号')"
-  export END_TIME=`date +'%Y-%m-%d %H:%M:%S'`
-  START_SECONDS=$(date --date="$START_TIME" +%s)
-  END_SECONDS=$(date --date="$END_TIME" +%s)
-  SECONDS=$((END_SECONDS-START_SECONDS))
-  HOUR=$(( $SECONDS/3600 ))
-  MIN=$(( ($SECONDS-${HOUR}*3600)/60 ))
-  SEC=$(( $SECONDS-${HOUR}*3600-${MIN}*60 ))
-  if [[ "${HOUR}" == "0" ]]; then
-    ECHOG "编译总计用时 ${MIN}分${SEC}秒"
-  else
-    ECHOG "编译总计用时 ${HOUR}时${MIN}分${SEC}秒"
-  fi
+  export TARGET_BSGET="$HOME_PATH/bin/targets/$TARGET_BOARD/$TARGET_SUBTARGET"
+  [[ -z "${TARGET_PROFILE}" ]] && TARGET_PROFILE="CONFIG_DIY/${matrixtarget}没有${CONFIG_FILE}文件,或者${CONFIG_FILE}文件内容为空"
 }
 
 function op_firmware() {
@@ -626,7 +370,7 @@ function openwrt_gitpull() {
     exit 1
   fi
   git pull
-  ECHOG "同步上游源码完毕,开始编译固件"
+  ECHOG "同步上游源码完毕,开始制作配置文件"
   source "${BUILD_PATH}/common.sh" && Diy_menu
 }
 
@@ -647,11 +391,6 @@ function op_again() {
   op_kongjian
   op_menuconfig
   make_defconfig
-  op_upgrade2
-  op_download
-  op_make
-  op_upgrade3
-  op_end
 }
 
 function openwrt_new() {
@@ -667,12 +406,6 @@ function openwrt_new() {
   op_diy_ip
   op_menuconfig
   make_defconfig
-  op_upgrade2
-  op_download
-  op_cpuxinghao
-  op_make
-  op_upgrade3
-  op_end
 }
 
 function menu() {
@@ -690,15 +423,14 @@ function menu() {
   clear
   clear
   echo
-  ECHOB "  请选择编译源码"
+  ECHOB "  请选择制作配置文件的源码"
   ECHOY " 1. Lede_${ledenh}内核,LUCI 18.06版本(Lede_source)"
   ECHOYY " 2. Lienol_${lienolnh}内核,LUCI Master版本(Lienol_source)"
   echo
   ECHOYY " 3. Immortalwrt_${tianlingnh}内核,LUCI 18.06版本(Tianling_source)"
   ECHOY " 4. Immortalwrt_${mortalnh}内核,LUCI 21.02版本(Mortal_source)"
   ECHOYY " 5. N1和晶晨系列CPU盒子专用(openwrt_amlogic)"
-  ECHOY " 6. 单独打包晶晨系列固件(前提是您要有armvirt的.tar.gz固件)"
-  ECHOYY " 7. 退出编译程序"
+  ECHOYY " 6. 退出编译程序"
   echo
   XUANZHEOP="请输入数字"
   while :; do
@@ -735,13 +467,6 @@ function menu() {
     break
     ;;
     6)
-      ECHOG "您选择了单独打包晶晨系列固件"
-      export matrixtarget="openwrt_amlogic"
-      op_busuhuanjing
-      op_amlogic
-    break
-    ;;
-    7)
       ECHOR "您选择了退出编译程序"
       exit 0
     break
@@ -755,9 +480,8 @@ function menu() {
 
 function Menu_requirements() {
   op_firmware > /dev/null 2>&1
-  source ${GITHUB_WORKSPACE}/OP_DIY/${matrixtarget}/settings.ini > /dev/null 2>&1
+  source ${GITHUB_WORKSPACE}/CONFIG_DIY/${matrixtarget}/settings.ini > /dev/null 2>&1
   tixing_op_config > /dev/null 2>&1
-  chenggong_op_config > /dev/null 2>&1
   cd ${GITHUB_WORKSPACE}
 }
 
@@ -767,19 +491,14 @@ function menuop() {
   echo
   echo
   echo -e " ${Blue}当前使用源码${Font}：${Green}${matrixtarget}${Font}"
-  echo -e " ${Blue}编译成功机型${Font}：${Green}${CG_PROFILE}${Font}"
-  echo -e " ${Blue}OP_DIY配置文件机型${Font}：${Green}${TARGET_PROFILE}${Font}"
+  echo -e " ${Blue}CONFIG_DIY配置文件机型${Font}：${Green}${TARGET_PROFILE}${Font}"
   echo
   echo
-  echo -e " 1${Green}.${Font}${Yellow}删除旧源码,使用[${matrixtarget}]源码全新编译${Font}(推荐)"
+  echo -e " 1${Green}.${Font}${Yellow}使用[${matrixtarget}]源码,再次制作配置文件${Font}"
   echo
-  echo -e " 2${Green}.${Font}${Yellow}保留缓存同步上游仓库源码,再次编译${Font}"
+  echo -e " 2${Green}.${Font}${Yellow}更换其他作者源码制作配置文件${Font}"
   echo
-  echo -e " 3${Green}.${Font}${Yellow}更换其他作者源码编译${Font}"
-  echo
-  echo -e " 4${Green}.${Font}${Yellow}打包N1和晶晨系列CPU固件${Font}"
-  echo
-  echo -e " 5${Green}.${Font}${Yellow}退出${Font}"
+  echo -e " 3${Green}.${Font}${Yellow}退出${Font}"
   echo
   echo
   XUANZHE="请输入数字"
@@ -787,23 +506,15 @@ function menuop() {
   read -p " ${XUANZHE}：" menu_num
   case $menu_num in
   1)
-    openwrt_new
-  break
-  ;;
-  2)
     Tishi="1"
     op_again
   break
   ;;
-  3)
+  2)
     menu
   break
-  ;;
-  4)
-    op_amlogic
-  break
   ;;   
-  5)
+  3)
     exit 0
     break
   ;;
@@ -814,49 +525,7 @@ function menuop() {
   done
 }
 
-function mecuowu() {
-  Menu_requirements
-  clear
-  echo
-  echo
-  echo -e " ${Yellow}您上回使用[${matrixtarget}]源码编译出现错误，请作如下选择${Font}"
-  echo
-  echo
-  echo -e " 1${Red}.${Font}${Blue}删除旧源码,继续使用[${matrixtarget}]源码全新编译${Font}"
-  echo
-  echo -e " 2${Red}.${Font}${Blue}使用旧源码继续编译(菜单)${Font}"
-  echo
-  echo -e " 3${Red}.${Font}${Blue}更换其他作者源码(菜单)${Font}"
-  echo
-  echo
-  XUANZHE="请输入数字"
-  while :; do
-  read -p " ${XUANZHE}：" menu_cuowu
-  case $menu_cuowu in
-  1)
-    ECHOG "开始以${matrixtarget}最新源码重新编译"
-    export matrixtarget="${matrixtarget}"
-    openwrt_new
-  break
-  ;;
-  2)
-    menuop
-  break
-  ;;
-  3)
-    menu
-    break
-  ;;
-  *)
-    XUANZHE="请输入正确的数字编号!"
-  ;;
-  esac
-  done
-}
-
-if [[ -f "${LOCAL_Build}/shibai" && -d "${GITHUB_WORKSPACE}/OP_DIY" ]]; then
-	mecuowu "$@"
-elif [[ -d "${HOME_PATH}/package" && -d "${HOME_PATH}/target" && -d "${HOME_PATH}/toolchain" && -f "${LOCAL_Build}/chenggong" && -d "${GITHUB_WORKSPACE}/OP_DIY" ]]; then
+if [[ -d "${HOME_PATH}/package" && -d "${HOME_PATH}/target" && -d "${HOME_PATH}/toolchain" && -d "${GITHUB_WORKSPACE}/CONFIG_DIY" ]]; then
 	menuop "$@"
 else
 	menu "$@"
