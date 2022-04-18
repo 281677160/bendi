24ad0b475cfbcd641dc721560d812923510a2a8f
281677160 <66651997+281677160@users.noreply.github.com>
2022-04-19 02:04:48 +0800

    Update local.sh

--- local.sh
+++ local.sh
@@ -233,6 +233,9 @@ function bianyi_xuanxiang() {
   [[ ! -d ${GITHUB_WORKSPACE}/OP_DIY ]] && op_diywenjian
   source $GITHUB_WORKSPACE/OP_DIY/${matrixtarget}/settings.ini
   if [[ "${EVERY_INQUIRY}" == "true" ]]; then
+    clear
+    echo
+    echo
     ECHOY "请在 OP_DIY/${matrixtarget} 里面设置好自定义文件"
     ZDYSZ="设置完毕后，按[W/w]回车继续编译"
     if [[ "${WSL_ubuntu}" == "YES" ]]; then
