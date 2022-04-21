46255315925280db251bfffe4b919134878f94e8
281677160 <66651997+281677160@users.noreply.github.com>
2022-04-21 11:33:25 +0800

    Update local.sh

--- local.sh
+++ local.sh
@@ -137,20 +137,16 @@ function op_common_sh() {
     sudo sh -c 'echo ClientAliveCountMax 6 >> /etc/ssh/sshd_config'
     sudo service ssh restart
   fi
-  if [[ -f ${COMMON_SH} ]]; then
-    source ${COMMON_SH} && Diy_update
+  curl -L https://raw.iqiq.io/281677160/common/main/common.sh > common.sh
+  if [[ $? -ne 0 ]];then
+    curl -L https://raw.githubusercontent.com/281677160/common/main/common.sh > common.sh
+  fi
+  if [[ $? -eq 0 ]];then
+    source common.sh && Diy_update
+    rm -fr common.sh
   else
-    curl -L https://raw.iqiq.io/281677160/common/main/common.sh > common.sh
-    if [[ $? -ne 0 ]];then
-      curl -L https://raw.githubusercontent.com/281677160/common/main/common.sh > common.sh
-    fi
-    if [[ $? -eq 0 ]];then
-      source common.sh && Diy_update
-      rm -fr common.sh
-    else
-      ECHOR "common文件下载失败，请检测网络后再用一键命令试试!"
-      exit 1
-    fi
+    ECHOR "common文件下载失败，请检测网络后再用一键命令试试!"
+    exit 1
   fi
 }
 
