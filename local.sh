87b6c6084076f728475bb91b835b38c88795b2af
281677160 <66651997+281677160@users.noreply.github.com>
2022-04-21 20:06:08 +0800

    Update local.sh

--- local.sh
+++ local.sh
@@ -201,7 +201,7 @@ function op_diywenjian() {
         EVERY_INQUIRY="true"                                    # 是否每次都询问您要不要去设置自定义文件（true=开启）（false=关闭）
         REGULAR_UPDATE="false"                                  # 把自动在线更新的插件编译进固件（在本地就是玩票性质）（true=开启）（false=关闭）
         Github="https://github.com/281677160/build-actions"     # 如果开启了‘把自动在线更新的插件编译进固件’，请设置好您的github地址
-      ' > "${X}"
+      ' >> "${X}"
       sed -i 's/^[ ]*//g' "${X}"
       sed -i '/^$/d' "${X}"
     done
@@ -231,7 +231,7 @@ function gengxin_opdiy() {
       EVERY_INQUIRY="true"                                    # 是否每次都询问您要不要去设置自定义文件（true=开启）（false=关闭）
       REGULAR_UPDATE="false"                                  # 把自动在线更新的插件编译进固件（在本地就是玩票性质）（true=开启）（false=关闭）
       Github="https://github.com/281677160/build-actions"     # 如果开启了‘把自动在线更新的插件编译进固件’，请设置好您的github地址
-    ' > "${X}"
+    ' >> "${X}"
     sed -i 's/^[ ]*//g' "${X}"
     sed -i '/^$/d' "${X}"
   done
