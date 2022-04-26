8267d9a4eb126bef7d26fddc92c03288aab654ce
281677160 <66651997+281677160@users.noreply.github.com>
2022-04-26 19:46:51 +0800

    Update amlogic.sh

--- amlogic.sh
+++ amlogic.sh
@@ -2,7 +2,7 @@
 git clone --depth 1 https://github.com/ophub/amlogic-s9xxx-openwrt.git amlogic
 mkdir -p amlogic/openwrt-armvirt
 if [[ `ls -1 "openwrt/bin/targets/armvirt/64" | grep -c ".*default-rootfs.tar.gz"` == '1' ]]; then
-  cp -Rf openwrt/bin/targets/armvirt/64/.*default-rootfs.tar.gz amlogic/openwrt-armvirt/openwrt-armvirt-64-default-rootfs.tar.gz && sync
+  cp -Rf openwrt/bin/targets/armvirt/64/*default-rootfs.tar.gz amlogic/openwrt-armvirt/openwrt-armvirt-64-default-rootfs.tar.gz && sync
 else
   armvirtargz="$(ls -1 "openwrt/bin/targets/armvirt/64" |grep ".*tar.gz" |awk 'END {print}')"
   cp -Rf openwrt/bin/targets/armvirt/64/${armvirtargz} amlogic/openwrt-armvirt/openwrt-armvirt-64-default-rootfs.tar.gz && sync
