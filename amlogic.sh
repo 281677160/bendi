15166705a1cbb4f2413d869580671acf76ca6fea
281677160 <66651997+281677160@users.noreply.github.com>
2022-04-26 18:43:05 +0800

    Update amlogic.sh

--- amlogic.sh
+++ amlogic.sh
@@ -19,9 +19,6 @@ OK="${Green}[OK]${Font}"
 ERROR="${Red}[ERROR]${Font}"
 
 # 变量
-cd ~
-export GITHUB_WORKSPACE="$PWD"
-export HOME_PATH="${GITHUB_WORKSPACE}/openwrt"
 
 function print_ok() {
   echo
@@ -105,8 +102,8 @@ fi
 
 
   cd ${GITHUB_WORKSPACE}
-  if [[ `ls -1 "${HOME_PATH}/bin/targets/armvirt/64" | grep -c "tar.gz"` == '0' ]]; then
-    mkdir -p "${HOME_PATH}/bin/targets/armvirt/64"
+  if [[ `ls -1 "openwrt/bin/targets/armvirt/64" | grep -c "tar.gz"` == '0' ]]; then
+    mkdir -p "openwrt/bin/targets/armvirt/64"
     clear
     echo
     echo
@@ -118,27 +115,27 @@ fi
     [[ "${WSL_ubuntu}" == "YES" ]] && explorer.exe .
     exit 1
   fi
-  if [[ ! -d "/home/dan/amlogic" ]]; then
+  if [[ ! -d "amlogic" ]]; then
     ECHOY "正在下载打包所需的程序,请耐心等候~~~"
-    git clone --depth 1 https://github.com/ophub/amlogic-s9xxx-openwrt.git ${GITHUB_WORKSPACE}/amlogic
+    git clone --depth 1 https://github.com/ophub/amlogic-s9xxx-openwrt.git amlogic
     judge "内核运行文件下载"
-    rm -rf ${GITHUB_WORKSPACE}/amlogic/{router-config,LICENSE,README.cn.md,README.md,.github,.git}
-  elif [[ -d "${GITHUB_WORKSPACE}/amlogic" ]] && [[  ! -f "${GITHUB_WORKSPACE}/amlogic/make" ]]; then
+    rm -rf amlogic/{router-config,LICENSE,README.cn.md,README.md,.github,.git}
+  elif [[ -d "amlogic" ]] && [[  ! -f "amlogic/make" ]]; then
     ECHOGG "发现已存在的打包程序缺少文件，请输入ubuntu密码删除打包程序重新下载"
-    sudo rm -rf "${GITHUB_WORKSPACE}/amlogic"
+    sudo rm -rf amlogic
     ECHOY "正在下载打包所需的程序,请耐心等候~~~"
-    git clone --depth 1 https://github.com/ophub/amlogic-s9xxx-openwrt.git ${GITHUB_WORKSPACE}/amlogic
+    git clone --depth 1 https://github.com/ophub/amlogic-s9xxx-openwrt.git amlogic
     judge "内核运行文件下载"
-    rm -rf ${GITHUB_WORKSPACE}/amlogic/{router-config,LICENSE,README.cn.md,README.md,.github,.git}
-  elif [[ -d "${GITHUB_WORKSPACE}/amlogic" ]] && [[  ! -d "${GITHUB_WORKSPACE}/amlogic/amlogic-s9xxx" ]]; then
+    rm -rf amlogic/{router-config,LICENSE,README.cn.md,README.md,.github,.git}
+  elif [[ -d "amlogic" ]] && [[  ! -d "amlogic/amlogic-s9xxx" ]]; then
     ECHOGG "发现已存在的打包程序缺少文件，请输入ubuntu密码删除打包程序重新下载"
-    sudo rm -rf "${GITHUB_WORKSPACE}/amlogic"
+    sudo rm -rf amlogic
     ECHOY "正在下载打包所需的程序,请耐心等候~~~"
-    git clone --depth 1 https://github.com/ophub/amlogic-s9xxx-openwrt.git ${GITHUB_WORKSPACE}/amlogic
+    git clone --depth 1 https://github.com/ophub/amlogic-s9xxx-openwrt.git amlogic
     judge "内核运行文件下载"
-    rm -rf ${GITHUB_WORKSPACE}/amlogic/{router-config,LICENSE,README.cn.md,README.md,.github,.git}
+    rm -rf amlogic/{router-config,LICENSE,README.cn.md,README.md,.github,.git}
   fi
-  [ -d amlogic/openwrt-armvirt ] || mkdir -p amlogic/openwrt-armvirt
+  [ ! -d amlogic/openwrt-armvirt ] && mkdir -p amlogic/openwrt-armvirt || sudo rm -rf amlogic/openwrt-armvirt/*
   ECHOY "全部可打包机型：s905x3_s905x2_s905x_s905w_s905d_s922x_s912"
   ECHOGG "设置要打包固件的机型[ 直接回车则默认全部机型 ]"
   export root_size="$(egrep -o ROOT_MB=\"[0-9]+\" "$GITHUB_WORKSPACE/amlogic/make" |cut -d "=" -f2 |sed 's/\"//g' )"
@@ -159,32 +156,31 @@ fi
   read -p " 请输入ROOTFS分区大小：" rootfs_size
   export rootfs_size=${rootfs_size:-"${root_size}"}
   ECHOYY "您设置的ROOTFS分区大小为：${rootfs_size}"
-  export make_size="$(egrep -o ROOT_MB=\"[0-9]+\" "$GITHUB_WORKSPACE/amlogic/make")"
+  export make_size="$(egrep -o ROOT_MB=\"[0-9]+\" "amlogic/make")"
   export zhiding_size="ROOT_MB=\"${rootfs_size}\""
-  sed -i "s?${make_size}?${zhiding_size}?g" "$GITHUB_WORKSPACE/amlogic/make"
   echo
   ECHOGG "请输入ubuntu密码进行固件打包程序"
-  sudo rm -rf ${GITHUB_WORKSPACE}/amlogic/out/*
-  sudo rm -rf ${GITHUB_WORKSPACE}/amlogic/openwrt-armvirt/*
-  sudo rm -rf ${GITHUB_WORKSPACE}/amlogic/amlogic-s9xxx/amlogic-kernel/*
-  if [[ `ls -1 "${HOME_PATH}/bin/targets/armvirt/64" | grep -c ".*default-rootfs.tar.gz"` == '1' ]]; then
-    cp -Rf ${HOME_PATH}/bin/targets/armvirt/64/.*default-rootfs.tar.gz ${GITHUB_WORKSPACE}/amlogic/openwrt-armvirt/openwrt-armvirt-64-default-rootfs.tar.gz && sync
+  [[ -d "amlogic/out" ]] && sudo rm -rf amlogic/out/*
+  [[ -d "amlogic/amlogic-s9xxx/amlogic-kernel" ]] && sudo rm -rf amlogic/amlogic-s9xxx/amlogic-kernel/*
+  if [[ `ls -1 "openwrt/bin/targets/armvirt/64" | grep -c ".*default-rootfs.tar.gz"` == '1' ]]; then
+    cp -Rf openwrt/bin/targets/armvirt/64/.*default-rootfs.tar.gz amlogic/openwrt-armvirt/openwrt-armvirt-64-default-rootfs.tar.gz && sync
   else
     armvirtargz="$(ls -1 "${HOME_PATH}/bin/targets/armvirt/64" |grep ".*tar.gz" |awk 'END {print}')"
-    cp -Rf ${HOME_PATH}/bin/targets/armvirt/64/${armvirtargz} ${GITHUB_WORKSPACE}/amlogic/openwrt-armvirt/openwrt-armvirt-64-default-rootfs.tar.gz && sync
+    cp -Rf openwrt/bin/targets/armvirt/64/${armvirtargz} amlogic/openwrt-armvirt/openwrt-armvirt-64-default-rootfs.tar.gz && sync
   fi
   if [[ `ls -a amlogic/openwrt-armvirt | grep -c "openwrt-armvirt-64-default-rootfs.tar.gz"` == '0' ]]; then
     print_error "amlogic/openwrt-armvirt文件夹没发现openwrt-armvirt-64-default-rootfs.tar.gz固件存在"
-    print_error "请检查${HOME_PATH}/bin/targets/armvirt/64文件夹内有没有openwrt-armvirt-64-default-rootfs.tar.gz固件存在"
+    print_error "请检查openwrt/bin/targets/armvirt/64文件夹内有没有openwrt-armvirt-64-default-rootfs.tar.gz固件存在"
     exit 1
   fi
   cd amlogic
+  sed -i "s?${make_size}?${zhiding_size}?g" make
   sudo chmod +x make
   sudo ./make -d -b ${amlogic_model} -k ${amlogic_kernel}
-  if [[ `ls -a ${GITHUB_WORKSPACE}/amlogic/out | grep -c "openwrt"` -ge '1' ]]; then
+  if [[ `ls -1 amlogic/out | grep -c "openwrt"` -ge '1' ]]; then
     print_ok "打包完成，固件存放在[amlogic/out]文件夹"
     if [[ "${WSL_ubuntu}" == "YES" ]]; then
-      cd ${GITHUB_WORKSPACE}/amlogic/out
+      cd amlogic/out
       explorer.exe .
       cd amlogic
     fi
