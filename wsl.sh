a4e9e2c48a3c9994b0e42f03f725afcffbb016b9
281677160 <66651997+281677160@users.noreply.github.com>
2022-11-18 18:10:03 +0800

    Update wsl.sh

--- wsl.sh
+++ wsl.sh
@@ -79,11 +79,6 @@ case "${UBUNTU_CODENAME}" in
 ;;
 esac
 
-if [[ `echo "${PATH}" |grep -c "Windows"` -eq '0' ]]; then
-  ECHOR "没有需要解决路径的问题"
-  exit 1
-fi
-
 if [[ `sudo grep -c "sudo ALL=(ALL:ALL) NOPASSWD:ALL" /etc/sudoers` == '0' ]]; then
   sudo sed -i 's?%sudo.*?%sudo ALL=(ALL:ALL) NOPASSWD:ALL?g' /etc/sudoers
 fi
@@ -105,6 +100,7 @@ sudo sh -c 'echo PasswordAuthentication yes >> /etc/ssh/sshd_config'
 sudo sh -c 'echo ClientAliveInterval 30 >> /etc/ssh/sshd_config'
 sudo sh -c 'echo ClientAliveCountMax 6 >> /etc/ssh/sshd_config'
 
+sudo sed -i '/grep -v inet6/d' ${GITHUB_WORKSPACE}/.bashrc
 sudo tee -a "${GITHUB_WORKSPACE}/.bashrc" << EOF > /dev/null
 echo "\$(ifconfig |grep inet |grep -v inet6 |grep -v 127.0.0.1|awk '{print \$(2)}')"
 EOF
@@ -138,4 +134,66 @@ else
 fi
 }
 
-ubuntu_WslPath "$@"
+function wsl_huanyuan() {
+sudo sed -i '/[interop]/d' /etc/wsl.conf
+sudo sed -i '/appendWindowsPath/d' /etc/wsl.conf
+if [[ `sudo grep -c "appendWindowsPath = false" /etc/wsl.conf` -ge '1' ]]; then
+  sudo sed -i '/[interop]/d' /etc/wsl.conf
+  sudo sed -i '/appendWindowsPath/d' /etc/wsl.conf
+fi
+
+if [[ `sudo grep -c "appendWindowsPath = false" /etc/wsl.conf` == '0' ]]; then
+  ECHOG "配置已更新，重启电脑即可"
+  exit 0
+else
+  ECHOG "无法完成操作请查看/etc/wsl.conf自行删除试试，删除以下2行代码"
+  ECHOG "删除：[interop]
+  ECHOG "删除：appendWindowsPath = false"
+  exit 1
+fi
+}
+
+
+function menu() {
+  clear
+  echo
+  echo
+  echo -e " 1${Red}.${Font}${Green}保留缓存,再次编译${Font}"
+  echo
+  echo -e " 2${Red}.${Font}${Green}重新选择源码编译${Font}"
+  echo
+  echo -e " 3${Red}.${Font}${Green}同步上游OP_DIY文件(不覆盖config配置文件)${Font}"
+  echo
+  echo -e " 4${Red}.${Font}${Green}退出${Font}"
+  echo
+  echo
+  XUANZop="请输入数字"
+  while :; do
+  read -p " ${XUANZop}：" menu_num
+  case $menu_num in
+  1)
+    ubuntu_WslPath
+  break
+  ;;
+  2)
+    wsl_huanyuan
+  break
+  ;;
+  3)
+    sudo sed -i '/grep -v inet6/d' ${GITHUB_WORKSPACE}/.bashrc
+    ECHOG "操作完成"
+  break
+  ;;
+  4)
+    echo
+    exit 0
+  break
+  ;;
+  *)
+    XUANZop="请输入正确的数字编号!"
+  ;;
+  esac
+  done
+}
+
+menu "$@"
