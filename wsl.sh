885bf6f65c39bd82b316acea194a8f4a42adaf0e
281677160 <66651997+281677160@users.noreply.github.com>
2022-11-21 20:53:35 +0800

    Update wsl.sh

--- wsl.sh
+++ wsl.sh
@@ -99,11 +99,11 @@ sudo sh -c 'echo PermitRootLogin yes >> /etc/ssh/sshd_config'
 sudo sh -c 'echo PasswordAuthentication yes >> /etc/ssh/sshd_config'
 sudo sh -c 'echo ClientAliveInterval 30 >> /etc/ssh/sshd_config'
 sudo sh -c 'echo ClientAliveCountMax 6 >> /etc/ssh/sshd_config'
-service ssh restart
+sudo service ssh restart
 
 sudo sed -i '/service ssh restart/d' ${GITHUB_WORKSPACE}/.bashrc
 sudo tee -a "${GITHUB_WORKSPACE}/.bashrc" << EOF > /dev/null
-service ssh restart
+sudo service ssh restart
 EOF
 
 sudo sed -i '/grep -v inet6/d' ${GITHUB_WORKSPACE}/.bashrc
