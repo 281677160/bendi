b202c9560329d4fd8dcd529b403b31adb32dfdc0
281677160 <66651997+281677160@users.noreply.github.com>
2023-05-31 04:06:10 +0800

    Update ceshi.sh

--- ceshi.sh
+++ ceshi.sh
@@ -436,10 +436,10 @@ fi
 }
 
 function Bendi_UpdateSource() {
-ECHOGG "更新插件源,请耐心等候..."
-cd ${HOME_PATH} && source ${GITHUB_ENV}
-source ${BUILD_PATH}/common.sh && Diy_menu3
-judge "更新插件源"
+# ECHOGG "更新插件源,请耐心等候..."
+# cd ${HOME_PATH} && source ${GITHUB_ENV}
+# source ${BUILD_PATH}/common.sh && Diy_menu3
+# judge "更新插件源"
 
 ECHOGG "加载自定义设置,请耐心等候..."
 cd ${HOME_PATH} && source ${GITHUB_ENV}
