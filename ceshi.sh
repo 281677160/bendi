c43b1dda8b1d838bbd0a9857728464b9611c67e8
281677160 <66651997+281677160@users.noreply.github.com>
2023-05-30 20:17:07 +0800

    Update ceshi.sh

--- ceshi.sh
+++ ceshi.sh
@@ -339,19 +339,7 @@ fi
 if [[ `grep -c "TIME" common.sh` -ge '1' ]]; then
   if [[ "${ZX_XZYM}" == "1" ]]; then
     REPEAT_EDLY="0"
-  elif [[ "${REPEAT_EDLY}" == "1" ]]; then
-    cd ${HOME_PATH}
-    LUCI_CHECKUT="$(git tag -l |grep '^V\|^v' |awk 'END {print}')"
-    if [[ -z "${LUCI_CHECKUT}" ]]; then
-      ECHOGG "同步上游源码"
-      git reset --hard HEAD^
-      git fetch --all
-      git reset --hard origin/${REPO_BRANCH2}
-      git pull
-      judge "同步上游源码"
-    fi
   fi
-  cd ${GITHUB_WORKSPACE}
   sudo chmod +x ${GITHUB_WORKSPACE}/common.sh
   source ${GITHUB_WORKSPACE}/common.sh && Diy_menu1
   sudo rm -rf ${GITHUB_WORKSPACE}/common.sh
